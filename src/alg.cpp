0929904703b227d43447383a57696ea3cdf38165
LizaShelkovskaya0705 <116936362+LizaShelkovskaya0705@users.noreply.github.com>
2023-02-23 19:14:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,45 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value <= 1)
+    return false;
+  for (int k = 2; k * k <= value; k++){
+    if (value % k == 0)
+      retur false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t chislo = 0;
+  uint64_t nomer = 0;
+  while (n > 0) {
+    if (checkPrime(chislo) == 1){
+      nomer = chislo;
+      --n;
+    }
+    ++chislo;
+  }
+  return nomer;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  while (true) {
+    value++;
+    if (checkPrime(value)){
+      return value;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t summa = 0;
+  hbound = hbound - 1;
+  while (hbound > 0) {
+    if (checkPrime(hbound)) {
+      summf = summa + hbound;
+    }
+    --hbound;
+  }
+  return summa;
 }
